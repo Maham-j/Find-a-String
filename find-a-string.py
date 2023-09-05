191ebe94069e2c5d27581c8c17ce32ec874ab0b2
Maham.J <139694482+Maham-j@users.noreply.github.com>
2023-09-05 21:44:06 +0500

    Create find-a-string.py

---
+++ find-a-string.py
@@ -0,0 +1,23 @@
+# Define a function that counts the occurrences of a sub_string within a string.
+def count_substring(string, sub_string):
+    counts = 0
+    l = len(sub_string)
+    
+    # Iterate through the string using a sliding window of length 'l'.
+    for i in range(len(string)):
+        # Check if the substring matches the current window.
+        if string[i:l+i] == sub_string:
+            counts += 1
+        
+    return counts
+
+if __name__ == '__main__':
+    # Read the main string and the sub-string from the user.
+    string = input().strip()
+    sub_string = input().strip()
+    
+    # Call the count_substring function to count occurrences.
+    count = count_substring(string, sub_string)
+    
+    # Print the count of sub-string occurrences.
+    print(count)
